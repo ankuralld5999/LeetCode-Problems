c9486a31ed80bcfb70ae64c5ec65cffebfa0b395
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-01-12 01:17:50 +0530

    Create Solution2.cpp

---
+++ LeetCode Problem-Set/10. Regular Expression Matching (Hard)/Solution2.cpp
@@ -0,0 +1,31 @@
+// Problem: https://leetcode.com/problems/regular-expression-matching/
+// Author: github.com/ankuralld5999
+// Time: O(MN)
+// Space: O(N)
+class Solution {
+    int M, N;
+    bool dfs(string &s, string &p, int i, int j) {
+        while (i < M && j < N) {
+            if (j + 1 < N && p[j + 1] == '*') {
+                if (dfs(s, p, i, j + 2)) return true;
+                while (i < M && (p[j] == '.' || s[i] == p[j])) {
+                    if (dfs(s, p, ++i, j + 2)) return true;
+                }
+                return false;
+            } else {
+                if (p[j] != '.' && s[i] != p[j]) return false;
+                ++i, ++j;
+            }
+        }
+        if (i == M) {
+            while (j + 1 < N && p[j + 1] == '*') j += 2;
+        }
+        return i == M && j == N;
+    }
+public:
+    bool isMatch(string s, string p) {
+        M = s.size(), N = p.size();
+        return dfs(s, p, 0, 0);
+    }
+};
+
