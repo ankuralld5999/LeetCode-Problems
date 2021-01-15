266b205ece4fa35b3c5eafa7c3ed8e48e6ae8475
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-01-15 17:30:02 +0530

    Create Solution2.cpp

---
+++ LeetCode Problem-Set/14. Longest Common Prefix (Easy)/Solution2.cpp
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/3sum/
+// Author: github.com/ankuralld5999
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        if (strs.size() == 1) return strs[0];
+        sort(strs.begin(), strs.end());
+        int len = 0;
+        while (strs[0][len] && strs.back()[len] && strs[0][len] == strs.back()[len]) ++len;
+        return strs[0].substr(0, len);
+    }
+};
