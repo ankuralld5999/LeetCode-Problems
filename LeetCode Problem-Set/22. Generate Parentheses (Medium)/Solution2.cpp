c4012064a03767a8719280c8e2c8b1b2a6268223
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-01-18 19:55:19 +0530

    Create Solution2.cpp

---
+++ LeetCode Problem-Set/22. Generate Parentheses (Medium)/Solution2.cpp
@@ -0,0 +1,28 @@
+// Problem: https://leetcode.com/problems/generate-parentheses/
+// Author: github.com/ankuralld5999
+// Time: O(C(N)) where C(N) is the Nth Catalan number
+// Space: O(N)
+class Solution {
+private:
+    vector<string> ans;
+    void generate(int leftCnt, int rightCnt, int i, string &s) {
+        if (i == s.size()) {
+            ans.push_back(s);
+            return;
+        }
+        if (leftCnt) {
+            s[i] = '(';
+            generate(leftCnt - 1, rightCnt, i + 1, s);
+        }
+        if (rightCnt > leftCnt) {
+            s[i] = ')';
+            generate(leftCnt, rightCnt - 1, i + 1, s);
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        string s(2 * n, '\0');
+        generate(n, n, 0, s);
+        return ans;
+    }
+};
