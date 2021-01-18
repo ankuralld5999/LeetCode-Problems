54fa60aefb3abc60b266c7e31f885c379cceb9ba
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-01-19 00:34:57 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/23. Merge k Sorted Lists (Hard)/Solution1.cpp
@@ -0,0 +1,27 @@
+// Problem: https://leetcode.com/problems/merge-k-sorted-lists/
+// Author: github.com/ankuralld5999
+// Time: O(NlogK)
+// Space: O(K)
+struct Cmp {
+    bool operator()(const ListNode *a, const ListNode *b) {
+        return a->val > b->val;
+    }
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode dummy(0), *tail = &dummy;
+        priority_queue<ListNode*, vector<ListNode*>, Cmp> q;
+        for (auto list : lists) {
+            if (list) q.push(list); // avoid pushing NULL list.
+        }
+        while (q.size()) {
+            auto node = q.top();
+            q.pop();
+            if (node->next) q.push(node->next);
+            tail->next = node;
+            tail = node;
+        }
+        return dummy.next;
+    }
+};
