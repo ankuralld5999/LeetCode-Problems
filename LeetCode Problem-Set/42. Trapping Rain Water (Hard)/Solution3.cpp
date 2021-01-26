4dcaf820696d5671d7f3fadec35916973e41c9e6
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-01-27 02:08:01 +0530

    Create Solution3.cpp

---
+++ LeetCode Problem-Set/42. Trapping Rain Water (Hard)/Solution3.cpp
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/trapping-rain-water/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    int trap(vector<int>& A) {
+        if (A.empty()) return 0;
+        int N = A.size(), ans = 0, i = 0, j = N - 1, left = 0, right = 0;
+        while (i < j) {
+            if (A[i] < A[j]) {
+                left = max(left, A[i]);
+                ans += left - A[i++];
+            } else {
+                right = max(right, A[j]);
+                ans += right - A[j--];
+            }
+        }
+        return ans;
+    }
+};
