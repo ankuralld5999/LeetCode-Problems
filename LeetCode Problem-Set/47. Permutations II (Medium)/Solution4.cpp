036a1c0878fff96a2a0dbb7b5898d54f0c43d2db
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-01-30 14:32:36 +0530

    Create Solution4.cpp

---
+++ LeetCode Problem-Set/47. Permutations II (Medium)/Solution4.cpp
@@ -0,0 +1,24 @@
+// Problem: https://leetcode.com/problems/permutations-ii
+// Author: github.com/ankuralld5999
+// Time: O(N!)
+// Space: O(1)
+class Solution {
+private:
+  bool nextPermutation(vector<int> &nums) {
+    int i = nums.size() - 2, j = nums.size() - 1;
+    while (i >= 0 && nums[i] >= nums[i + 1]) --i;
+    if (i >= 0) {
+        while (j > i && nums[j] <= nums[i]) --j;
+        swap(nums[i], nums[j]);
+    }
+    reverse(nums.begin() + i + 1, nums.end());
+    return i != -1;
+  }
+public:
+  vector<vector<int>> permuteUnique(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans { nums };
+    while (nextPermutation(nums)) ans.push_back(nums);
+    return ans;
+  }
+};
