b993b3c770fac98fa9a9cfdd6cce2e1576fdb9e7
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-01-31 14:28:34 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/48. Rotate Image (Medium)/Solution1.cpp
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/rotate-image/
+// Author: github.com/ankuralld5999
+// Time: O(N^2)
+// Space: O(1)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int N = matrix.size();
+        for (int i = 0; i < N / 2; ++i) {
+            for (int j = i; j < N - i - 1; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[N - j - 1][i];
+                matrix[N - j - 1][i] = matrix[N - i - 1][N - j - 1];
+                matrix[N - i - 1][N - j - 1] = matrix[j][N - i - 1];
+                matrix[j][N - i - 1] = tmp;
+            }
+        }
+    }
+};
