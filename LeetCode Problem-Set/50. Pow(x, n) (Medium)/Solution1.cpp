b9f40b6633e39193bf94cc2965065d1ab3da3583
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-01 11:36:49 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/50. Pow(x, n) (Medium)/Solution1.cpp
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/powx-n/
+// Author: github.com/ankuralld5999
+// Time: O(logN)
+// Space: O(logN)
+class Solution {
+private:
+    double myPow(double x, long n){
+        if(n<0) return 1/myPow(x, -n);
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(n==2) return x*x;
+        return myPow(myPow(x,n/2),2) * (n%2 ? x : 1);
+    }
+public:
+    double myPow(double x, int n) {
+        return myPow(x, (long)n);
+    }
+};
