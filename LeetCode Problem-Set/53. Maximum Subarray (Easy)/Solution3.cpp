da0802af8964d93bb0ce821333a3ddd2f1769257
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-02 00:18:21 +0530

    Create Solution3.cpp

---
+++ LeetCode Problem-Set/53. Maximum Subarray (Easy)/Solution3.cpp
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/maximum-subarray/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            nums[i] += max(nums[i - 1], 0);
+            ans = max(ans, nums[i]);
+        }
+        return ans;
+    }
+};
