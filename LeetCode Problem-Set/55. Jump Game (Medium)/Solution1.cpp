0111c8d117ea411e9cd6f30f7355dfe0884e2306
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-03 13:29:27 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/55. Jump Game (Medium)/Solution1.cpp
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/jump-game/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int last = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > last) return false;
+            last = max(last, i + nums[i]);
+        }
+        return true;
+    }
+};
