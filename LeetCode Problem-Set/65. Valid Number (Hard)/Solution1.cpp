6807517df1ae2f341effb30e6e82d29f2e954ca1
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-09 19:09:10 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/65. Valid Number (Hard)/Solution1.cpp
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/valid-number/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    bool isNumber(string s) {
+        int i = 0, N = s.size();
+        while (i < N && s[i] == ' ') ++i;
+        if (i < N && (s[i] == '+' || s[i] == '-')) ++i;
+        bool digitFound = false;
+        while (i < N && isdigit(s[i])) { ++i; digitFound = true; }
+        if (i < N && s[i] == '.') ++i;
+        while (i < N && isdigit(s[i])) { ++i; digitFound = true; }
+        if (!digitFound) return false;
+        if (i < N && s[i] == 'e') {
+            ++i;
+            if (i < N && (s[i] == '+' || s[i] == '-')) ++i;
+            bool expFound = false;
+            while (i < N && isdigit(s[i])) { ++i; expFound = true; }
+            if (!expFound) return false;
+        }
+        while (i < N && s[i] == ' ') ++i;
+        return i == N;
+    }
+};
