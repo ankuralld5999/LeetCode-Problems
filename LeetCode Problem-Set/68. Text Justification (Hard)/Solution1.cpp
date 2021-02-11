52bb7c61ae15ea3648879ed6078cc609987e2c0b
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-12 00:41:08 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/68. Text Justification (Hard)/Solution1.cpp
@@ -0,0 +1,31 @@
+// Problem: https://leetcode.com/problems/text-justification/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(N^2)
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string>res;
+        if(maxWidth == 0) return {""};
+        int i = 0, j = 0;
+        while(j != words.size()){
+            int len = -1;
+            while(j < words.size() && len + words[j].size() + 1 <= maxWidth)
+                len += words[j++].size() + 1;
+            int space = maxWidth - len + j - i - 1;
+            int k = i;
+            while(space){
+                words[k++] += " ";
+                space--;
+                if(j != words.size() && (k == j - 1 || k == j)) k = i;
+                if(j == words.size() && k == j) k = j - 1;
+            }
+            string line = "";
+            for(int l = i; l < j; l++)
+                line += words[l];
+            res.push_back(line);
+            i = j;
+        }
+        return res;
+    }
+};
