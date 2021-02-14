9342adb93ce930b037b981bfcdbd06337cc78203
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-14 23:37:42 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/71. Simplify Path (Medium)/Solution1.cpp
@@ -0,0 +1,24 @@
+// OJ: https://leetcode.com/problems/simplify-path/
+// Author: github.com/ankuralld5999
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        vector<string> s;
+        string name;
+        while (getline(ss, name, '/')) {
+            if (name == "..") {
+                if (s.size()) 
+                    s.pop_back();
+            }
+            
+            else if (name.size() && name != ".") 
+                s.push_back(name);
+        }
+        string ans;
+        for (auto n : s) 
+            ans += '/' + n;
+        
+        return ans.size() ? ans : "/";
+    }
+};
