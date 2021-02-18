a7cfb188923f9b3f957effebe7af89db3255a2f6
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-18 23:08:21 +0530

    Create Solution2.cpp

---
+++ LeetCode Problem-Set/76. Minimum Window Substring (Hard)/Solution2.cpp
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/minimum-window-substring/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(N)
+// Ref: https://discuss.leetcode.com/topic/30941/here-is-a-10-line-template-that-can-solve-most-substring-problems
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> m(128, 0);
+        for (char c : t) ++m[c];
+        int begin = 0, end = 0, head = 0, cnt = t.size(), len = INT_MAX;
+        while (end < s.size()) {
+            if (m[s[end++]]-- > 0) --cnt;
+            while (!cnt) {
+                if (end - begin < len) len = end - (head = begin);
+                if (m[s[begin++]]++ == 0) ++cnt;
+            }
+        }
+        return len == INT_MAX ? "" : s.substr(head, len);
+    }
+};
