e691a04ebb5897eff235e36976188e12a186faa0
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-24 22:46:34 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/82. Remove Duplicates from Sorted List II (Medium)/Solution1.cpp
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+  ListNode* deleteDuplicates(ListNode* head) {
+    ListNode dummy(0), *tail = &dummy;
+    while (head) {
+      if (head->next && head->next->val == head->val) {
+        int val = head->val;
+        while (head && head->val == val) {
+          ListNode *p = head;
+          head = head->next;
+          delete p;
+        }
+      } else {
+        tail->next = head;
+        tail = tail->next;
+        head = head->next;
+      }
+    }
+    tail->next = NULL;
+    return dummy.next;
+  }
+};
