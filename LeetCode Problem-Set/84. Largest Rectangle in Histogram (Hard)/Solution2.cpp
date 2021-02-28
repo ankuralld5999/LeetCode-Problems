b428f91c46c35a300e19c6a8af5ad52d1abb443d
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-02-28 10:12:28 +0530

    Create Solution2.cpp

---
+++ LeetCode Problem-Set/84. Largest Rectangle in Histogram (Hard)/Solution2.cpp
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/largest-rectangle-in-histogram/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(N)
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& A) {
+        A.push_back(0);
+        int N = A.size(), ans = 0;
+        stack<int> s;
+        for (int i = 0; i < N; ++i) {
+            while (s.size() && A[s.top()] >= A[i]) {
+                int h = A[s.top()];
+                s.pop();
+                int j = s.size() ? s.top() : -1;
+                ans = max(ans, h * (i - j - 1));
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
