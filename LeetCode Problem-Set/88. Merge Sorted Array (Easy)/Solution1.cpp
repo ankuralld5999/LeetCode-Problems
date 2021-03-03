f09f275029f79e487630e86c0bc68abba9f1a2f7
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-03-04 02:11:43 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/88. Merge Sorted Array (Easy)/Solution1.cpp
@@ -0,0 +1,17 @@
+
+// Problem: https://leetcode.com/problems/merge-sorted-array/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (k >= 0) {
+            if (j < 0 || i >= 0 && nums1[i] > nums2[j]) 
+                nums1[k--] = nums1[i--];
+            else 
+                nums1[k--] = nums2[j--];
+        }
+    }
+};
