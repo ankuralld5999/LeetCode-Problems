42f7130bd78bd737486442e847ffa27093c73f65
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-03-05 00:59:56 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/90. Subsets II (Medium)/Solution1.cpp
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/subsets-ii/
+// Author: github.com/ankuralld5999
+// Time: O(N^2 * 2^N)
+// Space: O(N)
+class Solution {
+private:
+    vector<vector<int>> ans;
+    void dfs(vector<int>& nums, int i, vector<int> &v) {
+        if (i == nums.size()) {
+            ans.push_back(v);
+            return;
+        }
+        v.push_back(nums[i]);
+        dfs(nums, i + 1, v);
+        v.pop_back();
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) ++i;
+        dfs(nums, i + 1, v);
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> v;
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0, v);
+        return ans;
+    }
+};
