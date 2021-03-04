04c19742b474cde3b50003c8e02aca8092040c5d
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-03-05 00:54:17 +0530

    Create Solution3.cpp

---
+++ LeetCode Problem-Set/89. Gray Code (Medium)/Solution3.cpp
@@ -0,0 +1,25 @@
+// Problem: https://leetcode.com/problems/gray-code/
+// Author: github.com/ankuralld5999
+// Time: O(2^N)
+// Space: O(1)
+class Solution {
+private:
+    inline int toggle(int val, int i) {
+        return val ^ (1 << i);
+    }
+    void grayCode(int n, int &val, vector<int> &v){
+        if (n < 0) return;
+        grayCode(n - 1, val, v);
+        val = toggle(val, n);
+        v.push_back(val);
+        grayCode(n - 1, val, v);
+    }
+public:
+    vector<int> grayCode(int n) {
+        vector<int> v;
+        int val = 0;
+        v.push_back(val);
+        grayCode(n - 1, val, v);
+        return v;
+    }
+};
