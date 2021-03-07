eec6e6426e38806bf875f24b57d6398f93e5631b
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-03-07 23:27:21 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set/92. Reverse Linked List II (Medium)/Solution1.cpp
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/reverse-linked-list-ii/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        ListNode dummy, *p = &dummy;
+        dummy.next = head;
+        for (int i = 1; i < m; ++i) p = p->next;
+        auto q = p->next, tail = q;
+        for (int i = m; i <= n; ++i) {
+            auto node = q;
+            q = q->next;
+            node->next = p->next;
+            p->next = node;
+        }
+        tail->next = q;
+        return dummy.next;
+    }
+};
