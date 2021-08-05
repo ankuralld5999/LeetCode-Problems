d36b6cdeee940ee7dbe7c002e49a0070a076d89e
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-08-05 21:55:18 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set(100-999)/Solution1.cpp
@@ -0,0 +1,10 @@
+// OJ: https://leetcode.com/problems/same-tree/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(H)
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return (!p && !q) || (p && q && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
