3ed1b8663bb4260d50afc772a1bfabe4d5f09a44
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-08-06 10:06:46 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set (100-999)/101. Symmetric Tree (Easy)/Solution1.cpp
@@ -0,0 +1,15 @@
+// Problem: https://leetcode.com/problems/symmetric-tree
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(H)
+class Solution {
+private:
+  bool isSymmetric(TreeNode *r1, TreeNode *r2) {
+    return (!r1 && !r2)
+    || (r1 && r2 && r1->val == r2->val && isSymmetric(r1->left, r2->right) && isSymmetric(r1->right, r2->left));
+  }
+public:
+  bool isSymmetric(TreeNode* root) {
+    return isSymmetric(root, root);
+  }
+};
