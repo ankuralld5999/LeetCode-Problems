4015f1390f7e932b70b1d5896944542d50fbd907
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-08-31 23:27:11 +0530

    Create Solution3.cpp

---
+++ LeetCode Problem-Set (100-999)/127. Word Ladder (Hard)/Solution3.cpp
@@ -0,0 +1,40 @@
+// Problem: https://leetcode.com/problems/word-ladder/
+// Author: github.com/ankuralld5999
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+        unordered_set<string> head, tail, *phead, *ptail;
+        head.insert(beginWord);
+        wordList.erase(beginWord);
+        tail.insert(endWord);
+        wordList.erase(endWord);
+        int d = 2;
+        while (!head.empty() && !tail.empty()) {
+            if (head.size() < tail.size()) {
+                phead = &head;
+                ptail = &tail;
+            } else {
+                phead = &tail;
+                ptail = &head;
+            }
+            unordered_set<string> tmp;
+            for (string s : *phead) {
+                for (int i = 0; i < s.size(); ++i) {
+                    char ch = s[i];
+                    for (int j = 0; j < 26; ++j) {
+                        s[i] = 'a' + j;
+                        if (ptail->find(s) != ptail->end()) return d;
+                        if (wordList.find(s) != wordList.end()) {
+                            tmp.insert(s);
+                            wordList.erase(s);
+                        }
+                    }
+                    s[i] = ch;
+                }
+            }
+            ++d;
+            *phead = tmp;
+        }
+        return 0;
+    }
+};
