9e5968d6cd81938e4a75cae1da7d2d2529837971
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-09-21 22:59:15 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set (100-999)/145. Binary Tree Postorder Traversal (Easy)/Solution1.cpp
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/binary-tree-postorder-traversal/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(H)
+class Solution {
+private:
+    vector<int> ans;
+    void dfs(TreeNode* root) {
+        if (!root) return;
+        dfs(root->left);
+        dfs(root->right);
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
