3a75fde125d6c28fd0588bce8dd1305f1e15a5eb
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-09-27 18:52:25 +0530

    Create Solution2.cpp

---
+++ LeetCode Problem-Set (100-999)/148. Sort List (Medium)/Solution2.cpp
@@ -0,0 +1,34 @@
+// Problem: https://leetcode.com/problems/sort-list/
+// Author: github.com/ankuralld5999
+class Solution {
+private:
+    void quickSort(ListNode *prevBegin, ListNode *begin, ListNode *end) {
+        if (begin == end) return;
+        ListNode *pivot = begin, *prevCur = pivot, *equalTail = pivot, *cur = pivot->next;
+        while (cur != end) {
+            if (cur->val < pivot->val) {
+                prevCur->next = cur->next;
+                cur->next = prevBegin->next;
+                prevBegin->next = cur;
+            } else if (cur->val == pivot->val && equalTail != prevCur) {
+                prevCur->next = cur->next;
+                cur->next = equalTail->next;
+                equalTail->next = cur;
+                equalTail = cur;
+            } else {
+                prevCur = cur;
+            }
+            cur = prevCur->next;
+        }
+        quickSort(prevBegin, prevBegin->next, pivot);
+        quickSort(equalTail, equalTail->next, end);
+    }
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;
+        quickSort(&dummy, head, NULL);
+        head = dummy.next;
+        return head;
+    }
+};
