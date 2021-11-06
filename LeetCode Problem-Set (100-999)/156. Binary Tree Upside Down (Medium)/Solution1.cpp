4dd66d2175e4e765cea6f38f980d192573d65fb8
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2021-11-06 18:59:39 +0530

    Create Solution1.cpp

---
+++ LeetCode Problem-Set (100-999)/156. Binary Tree Upside Down (Medium)/Solution1.cpp
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/binary-tree-upside-down/
+// Author: github.com/ankuralld5999
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if (!root || !root->left) return root;
+        TreeNode *left = root->left, *right = root->right, *newRoot = NULL;
+        while (left) {
+            TreeNode *nextLeft = left->left, *nextRight = left->right;
+            if (!newRoot) root->left = root->right = NULL;
+            newRoot = left;
+            newRoot->left = right;
+            newRoot->right = root;
+            root = left;
+            left = nextLeft;
+            right = nextRight;
+        }
+        return newRoot;
+    }
+};
